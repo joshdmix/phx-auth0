cd97048825799a0bb5f24aef65caa3402d26d1c0
jdm1986 <josh.mix@eltoro.com>
2019-06-15 21:15:41 -0400

    auth setup, need to fix :to auth/auth0 issue

--- lib/countdown_web/controllers/page_controller.ex
+++ lib/countdown_web/controllers/page_controller.ex
@@ -1,7 +1,9 @@
 defmodule CountdownWeb.PageController do
   use CountdownWeb, :controller
+  alias Countdown.Events
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    events = Events.list_future_events()
+    render(conn, "index.html", events: events, current_user: get_session(conn, :current_user))
   end
 end
--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Countdown.MixProject do
   def application do
     [
       mod: {Countdown.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:ueberauth, :ueberauth_auth0, :logger, :runtime_tools]
     ]
   end
 
@@ -36,13 +36,16 @@ defmodule Countdown.MixProject do
       {:phoenix, "~> 1.4.4"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
+      {:poison, "~> 3.1"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ueberauth, "~> 0.4"},
+      {:ueberauth_auth0, "~> 0.3"}
     ]
   end
 
